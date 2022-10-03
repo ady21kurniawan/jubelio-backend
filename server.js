7b9991295def5a8dbe57e21e2c70ab9f93b7e89d
idris <idris@PTs-MacBook-Pro.local>
2022-10-03 20:04:34 +0700

    set env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const Hapi = require('@hapi/hapi');
 const routes = require("./routes");
 const Path = require("path");
+require('dotenv').config()
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
+        port: process.env.PORT,
+        host: process.env.HOST,
         routes: {
             files: {
                 relativeTo: Path.join(__dirname, 'upload')
